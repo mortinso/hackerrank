83cb38a596f074575585ea54c410e7bd6af3c87a
mortinso <89762267+mortinso@users.noreply.github.com>
2025-11-19 15:54:48 +0000

    dw about it

--- C-Solutions/11_1D-arrays.c
+++ C-Solutions/11_1D-arrays.c
@@ -4,12 +4,12 @@
 int main() { 
     int n, tmp, sum = 0;
     scanf("%d", &n);
-    
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &tmp);
         sum += tmp;
     }
-    
+
     printf("%d", sum);
     return 0;
 }
